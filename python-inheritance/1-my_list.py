353fe75c56fa04dfc74ca7c45d718b12ec75d0f9
Samukelo Mngomezulu <110415118+SamuMngomezulu@users.noreply.github.com>
2025-08-06 08:39:50 +0200

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""my_list"""
+"""
+Module containing MyList class that inherits from list
+"""
 
 
 class MyList(list):
-    """my_list"""
+    """
+    MyList class that inherits from list with additional print_sorted method
+    """
 
     def print_sorted(self):
-        """print_sorted"""
+        """Prints the list in sorted ascending order"""
         print(sorted(self))
